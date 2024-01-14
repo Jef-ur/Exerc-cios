48a69ad9429fcef077a0402fab5c1c25ad553cb5
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-14 17:13:31 -0300

    Add files via upload

---
+++ maior_menor_ou_igual_zero.py
@@ -0,0 +1,12 @@
+numero = int( input( 'Digite um numero: '))
+
+print()
+if numero < 0:
+    print( f'O número { numero} é menor que zero.')
+
+elif numero > 0:
+    print( f'O número { numero} é maior que zero.')
+
+else:
+    print( f'O numero digitado é zero.')
+

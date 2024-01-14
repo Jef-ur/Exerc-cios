87f2beceb6cbfe40983bd487d4009da084e3687b
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-14 14:00:34 -0300

    Add files via upload

---
+++ operacoes_matematicas.py
@@ -0,0 +1,12 @@
+num1 = 3
+num2 = 2
+soma = num1 + num2
+subt = num1 - num2
+mult = num1 * num2
+divi = num1 / num2
+
+
+print( f'A soma entre { num1} e { num2} é { soma}.')
+print( f'A subtração ente { num1} e { num2} é { subt}.')
+print( f'A multiplicação entre { num1} e { num2} é { mult}.')
+print( f'E a divisão entre { num1} e { num2} é {divi}.')

d45d76cdb9ba70379f451c0da566c245ab9d5c79
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-14 16:12:42 -0300

    Add files via upload

---
+++ area_de_triagulo.py
@@ -0,0 +1,6 @@
+base = int( input( 'Costas da base em cm: '))
+altura = int( input( 'Altura em cm: '))
+area = ( base * altura) / 2
+
+print()
+print( f'Um triángulo com base { base}cm e altura { altura}cm tem área de { area}cm².')

ea778988f8510e25f2643592bafcfa34d2f4bf12
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-14 14:32:21 -0300

    Add files via upload

---
+++ verifica_numeros.py
@@ -0,0 +1,7 @@
+numero1 = int( input( 'Digite um numero: '))
+numero2 = int( input( 'Digite outro: '))
+igual = numero1 == numero2
+maior = numero1 > numero2
+
+print( f'São iguais: { igual}.')
+print( f'O primeiro é maior: { maior}.')

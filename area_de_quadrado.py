e43f1b3b55f2e351996691d67cfd5c8dc1b288aa
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-14 15:17:11 -0300

    Add files via upload

---
+++ area_de_quadrado.py
@@ -0,0 +1,6 @@
+lado = int( input( 'Comprimento do lado em cm: '))
+altura = int( input( 'Altura em cm: '))
+area = lado * altura
+
+print()
+print( f'Um quadrado com lado { lado}cm² e altura { altura}cm² tem a área de { area}cm².')

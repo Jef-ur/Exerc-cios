c3e7a07f8ae2725c9b484d8cfde928fc21aa61b4
Jef-Ur <98272669+Jef-ur@users.noreply.github.com>
2024-01-17 00:21:24 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -2,7 +2,7 @@ numero = int( input( 'Numero: '))
 
 print( f'A tabuada de { numero}.')
 print( '_' * 49)
-for n in range( 1, 10):
+for n in range( 1, 11):
     print( f'|{ numero} + { n:>2} = { numero + n:>2}|', end = '')
     print( f'|{ numero} * { n:>2} = { numero * n:>2}|', end = '')
     print( f'|{ numero} / { n:>2} = { numero / n:>2.2f}|', end = '')
